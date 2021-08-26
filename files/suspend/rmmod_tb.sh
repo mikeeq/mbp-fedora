b20741d77840c5ded921c17d27bb4f7c100ad4dc
mikeeq <miotk.mikolaj@gmail.com>
2021-08-26 10:16:14 +0200

    Add shelcheck for all scripts

--- files/suspend/rmmod_tb.sh
+++ files/suspend/rmmod_tb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ "${1}" = "pre" ]; then
-  modprobe -r apple_ib_tb hid_apple
+  modprobe -r apple_ib_tb
+	modprobe -r hid_apple
 elif [ "${1}" = "post" ]; then
-  modprobe hid_apple apple_ib_tb
+	modprobe hid_apple
+  modprobe apple_ib_tb
 fi
