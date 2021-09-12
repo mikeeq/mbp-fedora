ff1663540204d51eed6b90612866e06fc3bfb60a
Vojtěch Sázel <wojta@users.noreply.github.com>
2021-09-12 14:28:43 +0200

    fix for suspend script
    Current script was still causing my system to freeze after resume. (MacBookPro16,1)

--- files/suspend/rmmod_tb.sh
+++ files/suspend/rmmod_tb.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if [ "${1}" = "pre" ]; then
   modprobe -r apple_ib_tb
+  modprobe -r apple_ib_als
   modprobe -r hid_apple
 elif [ "${1}" = "post" ]; then
   modprobe hid_apple
+  modprobe apple_ib_als
   modprobe apple_ib_tb
 fi
