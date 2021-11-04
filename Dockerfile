addc47b2ba2750cdbb1bd82bfabb1265d497f537
mikeeq <miotk.mikolaj@gmail.com>
2021-11-04 16:45:01 +0100

    Test building fedora 35 on fedora 34 docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM fedora:35
+FROM fedora:34
 
 RUN dnf upgrade -y \
   && dnf install -y \
--- build_local.sh
+++ build_local.sh
@@ -2,9 +2,9 @@
 
 set -eu -o pipefail
 
-# docker build -t fedora_iso:35 .
-# DOCKER_IMAGE=fedora:35
-DOCKER_IMAGE=fedora_iso:35
+# docker build -t fedora_iso:34 .
+# DOCKER_IMAGE=fedora:34
+DOCKER_IMAGE=fedora_iso:34
 LIVECD_CACHE_PATH=/var/cache/live
 
 # docker pull ${DOCKER_IMAGE}
